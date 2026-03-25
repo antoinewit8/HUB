4938ada403d3b428ff313d3f1232f1029dc7071c
antoinewit8 <antoinewit8@gmail.com>
2026-03-25 15:14:26 +0100

    Update 3_🗺️_Carte_Manuelle.py

--- transport_hub/pages/3_🗺️_Carte_Manuelle.py
+++ transport_hub/pages/3_🗺️_Carte_Manuelle.py
@@ -96,7 +96,7 @@ if st.session_state["calc"]:
             server_url = MAP_SERVER_URL
         )
 
-        components.html(html_final, height=1200, scrolling=False)
+        components.html(html_final, height=1250, scrolling=False)
 
     except Exception as e:
         st.error(f"❌ Erreur chargement map.html : {e}")
